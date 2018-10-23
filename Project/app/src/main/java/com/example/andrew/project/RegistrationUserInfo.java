b861ce67ebb479107947b093515ccb404cb201ed
ianDesson <ian.desson@outlook.com>
2018-10-23 18:16:37 -0400

    Updated the Back-End

---
+++ Project/app/src/main/java/com/example/andrew/project/RegistrationUserInfo.java
@@ -0,0 +1,15 @@
+package com.example.andrew.project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegistrationUserInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_user_info);
+    }
+}
