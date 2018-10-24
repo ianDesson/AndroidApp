c47226b7be3238f1051a0c37d43b4edfec48c984
ianDesson <ian.desson@outlook.com>
2018-10-24 14:08:09 -0400

    Updated dependencies and UI for signup page

--- Project/app/src/main/java/com/example/andrew/project/Registration.java
+++ Project/app/src/main/java/com/example/andrew/project/Registration.java
@@ -31,9 +31,9 @@ public class Registration extends AppCompatActivity {
 
     public void createAdmin(View view) {
         adminCreated = true;
-        String id = databaseUsers.push().getKey();
+        //String id = databaseUsers.push().getKey();
         user = new Admin();
-        databaseUsers.child(id).setValue(user);
+        //databaseUsers.child(id).setValue(user);
         Intent intent = new Intent(Registration.this, RegistrationUserInfo.class);
         intent.putExtra("User", user);
         startActivity(intent);
