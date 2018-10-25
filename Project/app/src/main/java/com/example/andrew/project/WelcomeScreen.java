3bac3436472a39cc766bfbb92e85a74b51bd5d8a
ianDesson <ian.desson@outlook.com>
2018-10-24 22:11:29 -0400

    Added WelcomeScreen class

---
+++ Project/app/src/main/java/com/example/andrew/project/WelcomeScreen.java
@@ -0,0 +1,18 @@
+package com.example.andrew.project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeScreen extends AppCompatActivity{
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_screen);
+
+    }
+}
