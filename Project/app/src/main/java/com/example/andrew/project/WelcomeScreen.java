efbf4fc174f5f54e35b5cb1b8af9da333bd7fcfe
Rohan <rohanswheels@yahoo.ca>
2018-10-25 15:24:55 -0400

    Added backend for welcome screen

--- Project/app/src/main/java/com/example/andrew/project/WelcomeScreen.java
+++ Project/app/src/main/java/com/example/andrew/project/WelcomeScreen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class WelcomeScreen extends AppCompatActivity{
 
@@ -13,6 +14,19 @@ public class WelcomeScreen extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen);
-
+        TextView usernameDisplay = (TextView) findViewById(R.id.username_display);
+        TextView accountType = (TextView) findViewById(R.id.account_type);
+        Intent intent = getIntent();
+        User user = (User) intent.getSerializableExtra("User");
+        usernameDisplay.setText(user.getUsername());
+        if(user instanceof Admin){
+            accountType.setText("Admin");
+        }
+        else if(user instanceof HomeOwner){
+            accountType.setText("Home Owner");
+        }
+        else{
+            accountType.setText("Service Provider");
+        }
     }
 }
