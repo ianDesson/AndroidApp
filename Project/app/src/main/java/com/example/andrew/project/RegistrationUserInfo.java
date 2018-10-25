dfe776ce11ddfa65c393a256ccb128f8141fcb5c
mattmalek <mattjm007@gmail.com>
2018-10-25 01:12:07 -0400

    Added input validation for all fields, with features

--- Project/app/src/main/java/com/example/andrew/project/RegistrationUserInfo.java
+++ Project/app/src/main/java/com/example/andrew/project/RegistrationUserInfo.java
@@ -1,13 +1,26 @@
 package com.example.andrew.project;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class RegistrationUserInfo extends AppCompatActivity {
 
     private User user;
+
+    //Used to ensure no white spaces allowed in password input
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^(?=\\S+$)$");
+
+    private TextInputLayout textInputEmail;
+    private TextInputLayout textInputUsername;
+    private TextInputLayout textInputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +29,78 @@ public class RegistrationUserInfo extends AppCompatActivity {
 
         Intent intent = getIntent();
         user = (User) intent.getSerializableExtra("User");
+
+        //Added by Matt
+        textInputEmail = findViewById(R.id.text_input_email);
+        textInputUsername = findViewById(R.id.text_input_username);
+        textInputPassword = findViewById(R.id.text_input_password);
     }
 
-    public void continueButton (View view) {
-        
+    private boolean validateEmail() {
+        String emailInput = textInputEmail.getEditText().getText().toString().trim();
+
+        if (emailInput.isEmpty()) {
+            textInputEmail.setError("Field can't be empty");
+            return false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
+            textInputEmail.setError("Please enter a valid email address");
+            return false;
+        } else {
+            textInputEmail.setError(null);
+            //textInputEmail.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateUsername() {
+        String usernameInput = textInputUsername.getEditText().getText().toString().trim();
+        if (usernameInput.isEmpty()) {
+            textInputUsername.setError("Field can't be empty");
+            return false;
+        } else {
+            textInputUsername.setError(null);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String passwordInput = textInputPassword.getEditText().getText().toString().trim();
+
+        if (passwordInput.isEmpty()) {
+            textInputPassword.setError("Field can't be empty");
+            return false;
+        } else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
+            textInputPassword.setError("Password can't contain white spaces");
+            return false;
+        } else {
+            textInputPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void continueButton(View view) {
+        //get the text from the three fields
+        //store them as three dif strings
+        //search for the pass and username and make no spaces
+        //if it fails, show warning
+        //Do same thing for email
+        //IF ALL THOSE ARE TRUE
+        //Push to database
+        //Bring to the next page (welcome page)
+
+        //startActivity(newIntent(RegistrationUserInfo.this, WelcomeScreen.class));
+
+        if (!validateEmail() | !validateUsername() | !validatePassword()) {
+            return;
+        }
+
+        String input = "Email: " + textInputEmail.getEditText().getText().toString();
+        input += "\n";
+        input += "Username: " + textInputUsername.getEditText().getText().toString();
+        input += "\n";
+        input += "Password: " + textInputPassword.getEditText().getText().toString();
+
+        Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
+
     }
 }
